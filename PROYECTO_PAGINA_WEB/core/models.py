299d1ca1b9a65d5d9c006b51cce8f008c0fe0020
felirosalesb <feli.rosales@duocuc.cl>
2024-06-06 16:35:29 -0400

    DB creada

--- PROYECTO_PAGINA_WEB/core/models.py
+++ PROYECTO_PAGINA_WEB/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Producto(models.Model):
+    codigo = models.CharField(max_length=4, primary_key=True)
+    detalle = models.CharField(max_length=200)
+    precio = models.IntegerField()
+    stock = models.IntegerField()
+    oferta = models.BooleanField()
+    imagen = models.CharField(max_length=200)
