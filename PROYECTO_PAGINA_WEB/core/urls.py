4195f888ca23b494157699db97fe9c28db0955e0
Nicarvas1 <nicolas.icv99@gmail.com>
2024-06-17 15:17:08 -0400

    login

--- PROYECTO_PAGINA_WEB/core/urls.py
+++ PROYECTO_PAGINA_WEB/core/urls.py
@@ -1,12 +1,13 @@
 
 from django.urls import path
 from .views import *
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('', home, name="home"),
-    path('login', login, name="login"),
     path('productos', productos, name="productos"),
     path('contacto', contacto,name="contacto"),
     path('carrito', carrito,name="carrito"),
     path('usuario', usuario,name="usuario"),
+    path('login', LoginView.as_view(template_name= 'core/login.html'), name="login"),
 ]
