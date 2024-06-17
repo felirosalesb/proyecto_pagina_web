29d7f3b5888bce1c88d48c6d2258aba810f33d6b
felirosalesb <feli.rosales@duocuc.cl>
2024-06-17 14:46:43 -0400

    Que pasa mi gente

--- PROYECTO_PAGINA_WEB/core/urls.py
+++ PROYECTO_PAGINA_WEB/core/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
 from .views import *
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('', home, name="home"),
-    path('login', login, name="login"),
+    path('login', LoginView.as_view(template_name= 'core/login.html' ) , name="login"),
     path('productos', productos, name="productos"),
     path('contacto', contacto,name="contacto"),
     path('carrito', carrito,name="carrito"),
