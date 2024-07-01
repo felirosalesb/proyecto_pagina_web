40a61ab8b6dc9fac2f7bc26c2a1c5cf92cfe20fd
Nicolas Carmona Vásquez <nicolas.icv99@gmail.com>
2024-07-01 19:06:13 -0400

    tercera entrega lista

--- PROYECTO_PAGINA_WEB/core/urls.py
+++ PROYECTO_PAGINA_WEB/core/urls.py
@@ -5,6 +5,8 @@ from django.contrib.auth.views import LoginView
 from django.contrib.auth.views import LogoutView
 from . import views
 
+
+
 urlpatterns = [
     path('', home, name="home"),
     path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
@@ -14,7 +16,7 @@ urlpatterns = [
     path('usuario', usuario,name="usuario"),
     path('login', LoginView.as_view(template_name= 'core/login.html'), name="login"),
     path('registro', registro, name="registro"),
-  path('productos/', views.lista_productos, name='lista_productos'),
+    path('productos/', views.lista_productos, name='lista_productos'),
     path(r'^productos/(?P<producto_id>[\w-]+)/$', views.agregar_al_carrito, name='agregar_al_carrito'),
     path('productos/limpiar/', views.limpiar_carrito, name='limpiar_carrito'),
 ]
